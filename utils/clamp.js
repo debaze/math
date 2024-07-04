f641765dfd7c2f3ef717f78ab57464f8a44f7fe1
matteokeole <matteo@keole.net>
2024-07-04 20:22:53 +0200

    fix: Separate min/max functions for int/floats

--- utils/clamp.js
+++ utils/clamp.js
@@ -2,16 +2,40 @@
  * @param {Number} a
  * @param {Number} b
  */
-export function max(a, b) {
-	return a - (a - b & a - b >> 31);
+export function min(a, b) {
+	if (a < b) {
+		return a;
+	}
+
+	return b;
 }
 
 /**
  * @param {Number} a
  * @param {Number} b
  */
-export function min(a, b) {
+export function minInt(a, b) {
 	return b + (a - b & a - b >> 31);
+}
+
+/**
+ * @param {Number} a
+ * @param {Number} b
+ */
+export function max(a, b) {
+	if (a > b) {
+		return a;
+	}
+
+	return b;
+}
+
+/**
+ * @param {Number} a
+ * @param {Number} b
+ */
+export function maxInt(a, b) {
+	return a - (a - b & a - b >> 31);
 }
 
 /**
